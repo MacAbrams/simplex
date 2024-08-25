7d515706e8dad0b5364baa2d6712fd1edb32222d
Mac Abrams <fishbock@gmail.com>
2024-08-25 08:36:43 -0400

    basic transform

--- simplex.glsl
+++ simplex.glsl
@@ -1,3 +1,14 @@
 void main(){
-    frag_color = vec4(1.,0.,0.,1.);
+    vec2 uv = gl_FragCoord.xy/iResolution.xy;
+    uv.x*= iResolution.x/iResolution.y;
+    uv*=10.;
+    vec3 col = vec3(0.);
+    vec2 t = vec2(uv.x+uv.y/2.,uv.y*2./sqrt(3.));
+    vec2 f = fract(t);
+    vec2 al = floor(t);
+    vec2 bl = al+vec2(1.);
+    vec2 cl = al+(f.x>f.y?vec2(1.,0.):vec2(0.,1.));
+    col = vec3(f,f.x>f.y);
+
+    gl_FragColor = vec4(col,1.);
 }
